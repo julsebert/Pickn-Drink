f769b47cef96aff500f5bc1fa1f7b848e650a5f3
Götz Elisabeth <eg054@hdm-stuttgart.de>
2023-11-23 18:27:07 +0100

    JavaFXX eingerichtet

---
+++ src/main/java/mainpackage/Controller.java
@@ -0,0 +1,11 @@
+package mainpackage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void hello(ActionEvent event){
+        System.out.println("Hello");
+    }
+}
--- src/main/java/mainpackage/Driver.java
+++ src/main/java/mainpackage/Driver.java
@@ -11,17 +11,27 @@ import org.apache.logging.log4j.Logger;
 import java.net.URL;
 import java.util.Objects;
 
-public class Driver {
+public class Driver extends Application{
     private static Logger logger = LogManager.getLogger(Driver.class);
 
 
     public static void main(String[] args) {
 
         try {
-
+            launch(args);
         }catch(Exception e){
             logger.error("Something happened :/", e);
 
         }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL fxmlFileUrl = getClass().getClassLoader().getResource("sample.fxml");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(fxmlFileUrl));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+
     }
 }
