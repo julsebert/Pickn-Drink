c0fbf170840b55c0dd334332e2b6b6eb548b07ae
Götz Elisabeth <eg054@hdm-stuttgart.de>
2024-01-01 15:19:24 +0100

    Regestrierung mit csv datei speicherung und im login auslesen der csv datei, um auf Drinks zu kommen mit richtigen Daten

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module SE2StartupProject {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
+    requires com.opencsv;
 
     opens mainpackage;
     opens mainpackage.Logic;
